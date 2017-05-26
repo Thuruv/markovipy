15ba575fc468f0911177e600ae080949780a939d
Thuruv <Thuruv@users.noreply.github.com>
2017-05-26 17:24:06 +0530

    Revert "Minimizing the work on Proper casing"

--- markovipy/utils.py
+++ markovipy/utils.py
@@ -12,15 +12,19 @@ def fix_caps(word):
     :type word: <str>
     :return: <str>
     """
-    articles = ['a', 'an', 'of', 'the', 'is']
-    return ' '.join
-        (
-        [
-            word.capitalize() if  paragraph.index(word) == 0 else word.lower()
-            for word in paragraph.split(" ")
-        ]
-        )
-
+    if word.isupper() and word != "I":
+        """eg: word -> 'AAA', result -> 'aaa'
+        """
+        word = word.lower()
+    elif word[0].isupper():
+        """eg: word -> 'AvA', result -> 'Ava'
+        """
+        word = word.lower().capitalize()
+    else:
+        """eg: word -> 'aVA', result -> 'ava'
+        """
+        word = word.lower()
+    return word
 
 
 def get_word_list(file):
